ce04f69b7d041d97a093b655a0d7f74760a8a77a
sonalb-02 <sonalbulusu@gmail.com>
2026-04-03 06:14:08 +0000

    update

---
+++ pointer.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Function to swap two numbers using pointers
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+
+    printf("Enter two numbers:\n");
+    scanf("%d %d", &x, &y);
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+
+    swap(&x, &y);  // passing addresses
+
+    printf("After swapping: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
